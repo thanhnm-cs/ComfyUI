cd233b58b27bcb5767b6655777a091d06c9a049d
thanhnm777@gmail.com <thanhnm777@gmail.com>
2024-05-06 09:27:42 +0700

    modify run script

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 modelPath="/home/thanhnm2/ComfyUI/models/"
-lfsAbsPath="/backup/thanhnm2/comfyui/models/"
+lfsAbsPath="/data/thanhnm2/comfyui/models/"
+lfsAbsPathOutput="/data/thanhnm2/comfyui/output"
+
 
 docker build --tag comfyui:latest . 
 
 rsync -avz -rP ${modelPath} ${lfsAbsPath}
-docker run -v ${lfsAbsPath}:/workspace/models -d --gpus '"device=0,1"' --name="comfyui" comfyui:latest
+docker run -v ${lfsAbsPathOutput}:/workspace/output/ -v ${lfsAbsPath}:/workspace/models -d --gpus '"device=0,1"' --name="comfyui" comfyui:latest
 # docker run -v ${lfsAbsPath}:/workspace/models -it --gpus '"device=0,1"' --name="comfyui" comfyui:latest
